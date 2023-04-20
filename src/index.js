b673fb4cc6a2d0dd20146176c10ec615a9a9dc22
muhimin <muhimin01@users.noreply.github.com>
2023-04-20 13:42:48 +0800

    added: slash commands
    modified:   package.json
    modified:   src/index.js
    new file:   src/register-command.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const TOKEN = process.env.DISCORD_TOKEN;
 
 // requires the discord.js library. [$ npm install discord.js]
-const {Client, IntentsBitField} = require('discord.js');
+const { Client, IntentsBitField } = require('discord.js');
 
 // creates a new client with intents
 const client = new Client({
@@ -17,22 +17,42 @@ const client = new Client({
 
 // when client 'c' is ready, display a message to the console that the bot is live
 client.on('ready', (c) => {
-    console.log(`${c.user.tag} is live...`);
+    console.log(`${c.user.tag} is live!`);
 });
 
 // listens to contents of a message sent in a Discord channel
 client.on('messageCreate', (message) => {
     console.log(message.author + " says " + message.content); // logs the contents of the sent message
 
-    // this ignores messages sent by the bot, to avoid infinite loops. you could also use 'if(message.author.bot)' instead if you want to apply to all bots.
-    if(message.author.id === client.user.id) {
-        return;
-    } 
+    // this ignores messages sent by the bot (returns a null value early), to avoid infinite loops. you could also use 'if(message.author.bot)' instead if you want to apply to all bots.
+    if (message.author.id === client.user.id) return;
 
     // if the message contains 'Hello' or 'hello', reply with 'Sup
-    if (message.content ===  'hello') {
+    if (message.content === 'hello') {
         message.reply('Sup');
-    }
+    };
+
+    if (message.content === 'holy hell') {
+        message.reply('new response just dropped');
+    };
+});
+
+// event listener. triggered when a slash command is run.
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    // logs the name of the command that is triggered
+    console.log("A /" + interaction.commandName + " command is triggered.");
+
+    // if a '/hey' command is triggered, reply with 'Hey!'
+    if (interaction.commandName === 'hey') {
+        interaction.reply('Hey!');
+    };
+
+    // marco, polo
+    if (interaction.commandName === 'marco') {
+        interaction.reply('polo!');
+    };
 });
 
 // runs the bot using the TOKEN constant
---
+++ src/register-command.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const TOKEN = process.env.DISCORD_TOKEN;
+const GUILD_ID = process.env.GUILD_ID;
+const CLIENT_ID = process.env.CLIENT_ID;
+
+const { REST, Routes } = require('discord.js');
+
+// an array of objects. this will be the list of commands with their descriptions. add/remove commands here.
+const commands = [
+    {
+        name: 'hey',
+        description: 'Replies with "Hey!"'
+    },
+    {
+        name: 'marco',
+        description: 'polo'
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+// function that registers the slash command
+(async () => {
+    try {
+        console.log('Registering slash commands...')
+
+        await rest.put(
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+            { body: commands }
+        )
+        console.log('Slash commands registered successfully!')
+
+    } catch (error) {
+        console.log(`An error occurred: ${error}`)
+    }
+})();
